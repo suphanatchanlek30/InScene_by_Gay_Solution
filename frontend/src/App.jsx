235021ae6f870043848e37d01e4b634e20617c06
suphanatchanlek <suphanatchanlek@gmail.com>
2024-11-06 18:13:52 +0700

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
 import './App.css';
-import Navbar from './components/layout/Navbar';
-import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return(
-    <Router>
-      <div className="min-h-screen flex flex-col p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+    <div>
 
-    </Router>
+    </div>
   );
 }
 
