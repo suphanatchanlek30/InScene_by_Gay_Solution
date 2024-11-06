ae7658a66f8ae818d3576b1425364cff0d0cab30
suphanatchanlek <suphanatchanlek@gmail.com>
2024-11-06 17:03:43 +0700

    add componens Profile_user

---
+++ frontend/src/components/Profile_user.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import user_test from "../assets/Img_test_Profile_user/user_test.svg";
+
+const Profile_user = () => {
+    // ข้อมูลแบบ hardcoded
+    const user = {
+        name: "Arya Muller"
+    };
+
+    return (
+
+      <div className="p-4 font-body">
+        {/* กรอบข้อมูล */}
+        <div className='flex flex-row items-center justify-start gap-2'>
+            {/* รูป Profile */}
+            <img src={user_test} alt="user_test" className='w-[37px] h-[37px] object-cover'/>
+            {/* ข้อมูล */}
+            <p className="text-[#1B1E28] text-[14px] font-medium">{user.name}</p>
+        </div>
+      </div>
+  )
+}
+
+export default Profile_user
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Profile_user from '../components/Profile_user'
+
+const Home = () => {
+  return (
+    <div>
+        <Profile_user/>
+    </div>
+  )
+}
+
+export default Home
